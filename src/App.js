e2d534c59b468b6a0f7a487a9ddded32f831ebb5
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2025-06-04 18:07:39 +0300

    delete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { useState } from "react";
 import "./App.css";
-import FirstModal from "./FirstModal";
-import ModalForm from "./ModalForm";
 
 function App() {
-  const [firstModal, setFirstModal] = useState(true);
-  return (
-    <div className="App">
-      {firstModal && <FirstModal setFirstModal={setFirstModal} />}
-      {!firstModal && <ModalForm />}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
